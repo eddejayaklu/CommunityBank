7e42b58efa2014d04b266bcb00fe033e43f80d5b
eddejayaklu <jayaedde@gmail.com>
2024-06-13 11:11:15 +0530

    added validations

--- src/main/java/com/mybank/accounts/controller/AccountController.java
+++ src/main/java/com/mybank/accounts/controller/AccountController.java
@@ -2,19 +2,24 @@ package com.mybank.accounts.controller;
 
 
 import com.mybank.accounts.constants.AccountsConstants;
+import com.mybank.accounts.dto.CustomerAndAccountDto;
 import com.mybank.accounts.dto.CustomerDto;
 import com.mybank.accounts.dto.ResponseDto;
+import com.mybank.accounts.mapper.CustomerAndAccountMapper;
 import com.mybank.accounts.service.IAccountsService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api",produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountController {
 
     private IAccountsService iAccountsService;
@@ -29,5 +34,44 @@ public class AccountController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerAndAccountDto> fetchAccount(@RequestParam
+                                                                  @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                                  String mobileNumber) {
+        CustomerAndAccountDto customerAndAccountDto= iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customerAndAccountDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerAndAccountDto customerAndAccountDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerAndAccountDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+                                                                @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                                String mobileNumber) {
+        boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
+        }
+    }
 
 }
--- src/main/java/com/mybank/accounts/repository/AccountRepository.java
+++ src/main/java/com/mybank/accounts/repository/AccountRepository.java
@@ -1,9 +1,18 @@
 package com.mybank.accounts.repository;
 
 import com.mybank.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Accounts,Long> {
+    Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
