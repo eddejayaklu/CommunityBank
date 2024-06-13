c6e0d57844867d282b56d4283f8e2522c5bf20cb
eddejayaklu <jayaedde@gmail.com>
2024-06-13 11:06:15 +0530

    aduting for create and update

--- src/main/java/com/mybank/accounts/AccountsApplication.java
+++ src/main/java/com/mybank/accounts/AccountsApplication.java
@@ -1,9 +1,13 @@
 package com.mybank.accounts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mybank/accounts/dto/CustomerDto.java
+++ src/main/java/com/mybank/accounts/dto/CustomerDto.java
@@ -1,10 +1,21 @@
 package com.mybank.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Size(min=5,max = 30,message = "Length of the name should be min 5 and max 30")
     private String name;
+
+    @NotEmpty(message = "email cannot be null or empty")
+    @Email(message = "email address should be valid")
     private String email;
+
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 }
