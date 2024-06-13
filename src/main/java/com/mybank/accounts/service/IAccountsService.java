b179ae4fe0286efb50981a09cb9bca685645649f
eddejayaklu <jayaedde@gmail.com>
2024-06-13 11:09:38 +0530

    reamining crud rest api operations

--- src/main/java/com/mybank/accounts/service/IAccountsService.java
+++ src/main/java/com/mybank/accounts/service/IAccountsService.java
@@ -1,7 +1,12 @@
 package com.mybank.accounts.service;
 
+import com.mybank.accounts.dto.CustomerAndAccountDto;
 import com.mybank.accounts.dto.CustomerDto;
 
 public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
+    CustomerAndAccountDto fetchAccount(String mobileNumber);
+    boolean updateAccount(CustomerAndAccountDto customerAndAccountDto);
+    boolean deleteAccount(String mobileNumber);
+
 }
