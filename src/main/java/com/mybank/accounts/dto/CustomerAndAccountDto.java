75f24def7182426d59dbdf425917b68640ac7c33
eddejayaklu <jayaedde@gmail.com>
2024-06-15 11:18:44 +0530

    With OPENAPI Rest documenation for DTO classes

--- src/main/java/com/mybank/accounts/dto/CustomerAndAccountDto.java
+++ src/main/java/com/mybank/accounts/dto/CustomerAndAccountDto.java
@@ -1,9 +1,20 @@
 package com.mybank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerAndAccountDto {
+    @Schema(
+            description = " Details of the Customer"
+    )
     private CustomerDto customer;
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDto Account;
 }
--- src/main/java/com/mybank/accounts/dto/CustomerDto.java
+++ src/main/java/com/mybank/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.mybank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -7,15 +8,28 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer"
+)
 public class CustomerDto {
+    @Schema(
+            description = "Name of the customer", example = "Eazy Bytes"
+    )
     @NotEmpty(message = "Name cannot be null or empty")
     @Size(min=5,max = 30,message = "Length of the name should be min 5 and max 30")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer", example = "abc@gmail.com"
+    )
     @NotEmpty(message = "email cannot be null or empty")
     @Email(message = "email address should be valid")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 }
